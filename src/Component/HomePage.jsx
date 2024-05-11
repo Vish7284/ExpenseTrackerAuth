71914ab76bc8740d91735ae923da1655e05aa03a
Vish7284 <vp75240194465@gmail.com>
2024-05-11 15:38:35 +0530

    verfyEmail

--- src/Component/HomePage.jsx
+++ src/Component/HomePage.jsx
@@ -1,6 +1,33 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom/cjs/react-router-dom";
+import ExpenseContext from "../store/expense-context";
 
 const HomePage = () => {
+    const ctx = useContext(ExpenseContext);
+
+    const verifyEmailHandler =async()=>{
+
+     try  { const response = await fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyC8idrG0OBLxrDZD1cJhoo2Z2VVhsnEFYc",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              requestType: "VERIFY_EMAIL",
+              idToken:ctx.token,
+            }),
+          }
+        );
+
+        if(!response.ok){
+            const errDat = await response.json();
+            throw new Error("nhi bhejenge verify email kya....")
+        }
+        const data = await response.json();
+        console.log(data.email);
+        }catch(err){
+            console.log(err);
+        }
+    }
    return (
      <div className="bg-orange-200 flex justify-between items-center px-4 py-2">
        <header className="flex justify-between w-full">
@@ -9,6 +36,11 @@ const HomePage = () => {
            <NavLink to="/userdetail" className="font-bold">Complete User Details</NavLink>
          </span>
        </header>
+       <body>
+        <div>
+            <button className="bg-purple-300 hover:bg-purple-600 rounded-lg" onClick={verifyEmailHandler}>Verify Email</button>
+        </div>
+       </body>
      </div>
    );
 };
