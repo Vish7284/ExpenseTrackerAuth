418d52d008d5247fa7dd27e38d15748a364b1638
Vish7284 <vp75240194465@gmail.com>
2024-05-15 14:08:13 +0530

    classBasedRedux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,7 @@
 import store from '../store';
 import classes from './Counter.module.css';
-import { useSelector ,useDispatch} from 'react-redux';
+import { useSelector ,useDispatch ,connect} from 'react-redux';
+import { Component } from 'react';
 
 const Counter = () => {
   const Dispatch = useDispatch()
@@ -27,3 +28,44 @@ const Counter = () => {
 };
 
 export default Counter;
+
+
+// class Counter extends Component{
+//   incrementHandler (){
+// this.props.increment();
+//   }
+//   decrementHandler(){
+//     this.props.decrement()
+//   };
+//   toggleCounterHandler(){}
+
+//   render(){
+//      return(
+//       <main className={classes.counter}>
+//        <h1>Redux Counter</h1>
+//        <div className={classes.value}>{this.props.counter}</div>
+//        <div>
+//          <button onClick={this.incrementHandler.bind(this)}>IncrementBY5</button>
+//          <button onClick={this.decrementHandler.bind(this)}>DecrementBY5</button>
+//        </div>
+//        <button onClick={this.toggleCounterHandler.bind(this)}>Toggle Counter</button>
+//      </main>
+//      )
+//   }
+// }
+
+// const mapStateToProps =(state)=>{
+
+//   return {
+//     counter : state.counter
+//   }
+// }
+
+// const mapDispatchToProps =(dispatch)=>{
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// }
+
+// export default connect(mapStateToProps,mapDispatchToProps)(Counter);
