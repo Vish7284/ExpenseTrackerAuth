01025b9bef7590ad7d8db7b0ae8bffbfc7788805
Vish7284 <vp75240194465@gmail.com>
2024-05-19 13:26:19 +0530

    addTOcart&&remove handled

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,17 +1,18 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch,useSelector} from 'react-redux';
 import classes from './CartButton.module.css';
 import { cartActions } from '../../store/cart';
 const CartButton = (props) => {
 
+  const quantity = useSelector((state) => state.cart.totalQuantity);
   const dispatch = useDispatch();
   const cartToggleHandler =()=>{
     dispatch(cartActions.setShow())
-    console.log(cartActions,"KYU NHI HO RHA HAI");
+    console.log(cartActions);
   }
   return (
-    <button className={classes.button}>
-      <span onClick={cartToggleHandler}>My Cart</span>
-      <span className={classes.badge}>1</span>
+    <button className={classes.button} onClick={cartToggleHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{quantity}</span>
     </button>
   );
 };
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,42 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
+const DUMMY_DATA = [
+  {
+    id: "p1",
+    price: 250,
+    title: "Whey Protien Isolate",
+    description: "A Product which comes on your trust of purity",
+  },
+  {
+    id: "p2",
+    price: 200,
+    title: "Whey Protien concentrate",
+    description: "A Product which comes on your trust of purity",
+  },
+  {
+    id: "p3",
+    price: 100,
+    title: "Creatine Monohyderate",
+    description: "A Product which comes on your trust of purity",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_DATA.map((product) => {
+          return (
+            <ProductItem
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              price={product.price}
+              description={product.description}
+            />
+          );
+        })}
       </ul>
     </section>
   );
